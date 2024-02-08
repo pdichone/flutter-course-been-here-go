ec727382623467259b54196bb0dce61e75e65677
Paulo <pdichone@gmail.com>
2024-02-08 12:32:50 -0800

    Refactor Form

---
+++ lib/components/add_place_form.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AddPlaceFrom extends StatefulWidget {
+  final String latitude;
+  final String longitude;
+  final String currentAddress;
+
+  final XFile? imageFile;
+
+  const AddPlaceFrom({
+    super.key,
+    required this.latitude,
+    required this.longitude,
+    required this.currentAddress,
+    this.imageFile,
+  });
+
+  @override
+  State<AddPlaceFrom> createState() => _AddPlaceFromState();
+}
+
+class _AddPlaceFromState extends State<AddPlaceFrom> {
+  final _formKey = GlobalKey<FormState>();
+  final _nameController = TextEditingController();
+  final _descriptionController = TextEditingController();
+  final _thoughtsController = TextEditingController();
+  double _rating = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextFormField(
+                  controller: _nameController,
+                  decoration: const InputDecoration(
+                    labelText: 'Place Name',
+                  ),
+                  validator: (value) {
+                    // if (value!.isEmpty) {
+                    //   return 'Please enter a name';
+                    // } else {
+                    //   return null;
+                    // }
+                  },
+                ),
+                TextFormField(
+                  controller: _descriptionController,
+                  decoration: const InputDecoration(
+                    labelText: 'Description',
+                  ),
+                  validator: (value) {
+                    // if (value!.isEmpty) {
+                    //   return 'Please enter a description';
+                    // } else {
+                    //   return null;
+                    // }
+                  },
+                ),
+                TextFormField(
+                  controller: _descriptionController,
+                  decoration: const InputDecoration(
+                    labelText: 'Thoughts',
+                  ),
+                  validator: (value) {
+                    // if (value!.isEmpty) {
+                    //   return 'Please enter your thoughts';
+                    // } else {
+                    //   return null;
+                    // }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Slider(
+                    value: _rating,
+                    min: 1,
+                    max: 5,
+                    divisions: 4,
+                    label: 'Rating: $_rating',
+                    onChanged: (double value) {
+                      // setState(() {
+                      //   _rating = value;
+                      // });
+                    }),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      // save place
+                    },
+                    child: Text('Save Place'))
+              ],
+            ),
+          ),
+        ));
+  }
+}
--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:been_here_go/components/add_place_form.dart';
 import 'package:been_here_go/components/camera_button.dart';
 import 'package:been_here_go/components/location_info.dart';
 import 'package:been_here_go/providers/auth_provider.dart';
@@ -19,11 +20,6 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   XFile? _selectedImage;
-  final _formKey = GlobalKey<FormState>();
-  final _nameController = TextEditingController();
-  final _descriptionController = TextEditingController();
-  final _thoughtsController = TextEditingController();
-  double _rating = 1;
   LocationData? _position;
   String _address = '';
 
@@ -131,79 +127,13 @@ class _HomePageState extends State<HomePage> {
             CameraButton(content: content),
 
             // Form Area
-            Form(
-                key: _formKey,
-                child: SingleChildScrollView(
-                  child: Padding(
-                    padding: const EdgeInsets.all(16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        TextFormField(
-                          controller: _nameController,
-                          decoration: const InputDecoration(
-                            labelText: 'Place Name',
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Please enter a name';
-                            } else {
-                              return null;
-                            }
-                          },
-                        ),
-                        TextFormField(
-                          controller: _descriptionController,
-                          decoration: const InputDecoration(
-                            labelText: 'Description',
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Please enter a description';
-                            } else {
-                              return null;
-                            }
-                          },
-                        ),
-                        TextFormField(
-                          controller: _descriptionController,
-                          decoration: const InputDecoration(
-                            labelText: 'Thoughts',
-                          ),
-                          validator: (value) {
-                            if (value!.isEmpty) {
-                              return 'Please enter your thoughts';
-                            } else {
-                              return null;
-                            }
-                          },
-                        ),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        Slider(
-                            value: _rating,
-                            min: 1,
-                            max: 5,
-                            divisions: 4,
-                            label: 'Rating: $_rating',
-                            onChanged: (double value) {
-                              setState(() {
-                                _rating = value;
-                              });
-                            }),
-                        const SizedBox(
-                          height: 20,
-                        ),
-                        ElevatedButton(
-                            onPressed: () {
-                              // save place
-                            },
-                            child: Text('Save Place'))
-                      ],
-                    ),
-                  ),
-                ))
+            _position != null
+                ? AddPlaceFrom(
+                    latitude: _position!.latitude.toString(),
+                    longitude: _position!.longitude.toString(),
+                    currentAddress: _address,
+                  )
+                : const CircularProgressIndicator()
           ],
         ),
       ),

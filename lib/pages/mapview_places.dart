8db5228fd4d8bf14e50a1bffb1876da9f07d2fa8
Paulo <pdichone@gmail.com>
2024-02-08 23:02:27 -0800

    Add markers on the map

--- lib/pages/mapview_places.dart
+++ lib/pages/mapview_places.dart
@@ -1,5 +1,8 @@
+import 'package:been_here_go/providers/auth_provider.dart';
+import 'package:been_here_go/providers/places_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:provider/provider.dart';
 
 class PlacesMapView extends StatefulWidget {
   const PlacesMapView({super.key});
@@ -10,9 +13,44 @@ class PlacesMapView extends StatefulWidget {
 
 class _PlacesMapViewState extends State<PlacesMapView> {
   late GoogleMapController? mapController;
-
+  final Map<String, Marker> _markers = {};
   Future<void> _onMapCreated(GoogleMapController controller) async {
     mapController = controller;
+  }
+
+  @override
+  void initState() {
+    final userId = Provider.of<AuthProvider>(context, listen: false).user!.uid;
+    final placesProvider = Provider.of<PlacesProvider>(context, listen: false);
+
+    placesProvider.fetchPlaces(userId).then((value) {
+      setState(() {
+        _markers.clear();
+        for (var place in value) {
+          final marker = Marker(
+              markerId: MarkerId(place.docId!),
+              position: LatLng(double.parse(place.latitude!),
+                  double.parse(place.longitude!)),
+              infoWindow:
+                  InfoWindow(title: place.name, snippet: place.description));
+
+          _markers[place.docId!] = marker;
+          // print('Places found::: ${place.latitude}');
+        }
+      });
+
+      // zom to the first marker when places are loaded
+      if (_markers.isNotEmpty) {
+        final firstMarker = _markers.entries.first;
+        final initialCameraPosition =
+            CameraPosition(target: firstMarker.value.position, zoom: 13);
+
+        mapController?.animateCamera(
+            CameraUpdate.newCameraPosition(initialCameraPosition));
+      }
+    });
+
+    super.initState();
   }
 
   @override
@@ -23,9 +61,10 @@ class _PlacesMapViewState extends State<PlacesMapView> {
         title: const Text('Places You\'ve Been'),
       ),
       body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition:
-              const CameraPosition(target: center, zoom: 10)),
+        onMapCreated: _onMapCreated,
+        initialCameraPosition: const CameraPosition(target: center, zoom: 10),
+        markers: _markers.values.toSet(),
+      ),
     );
   }
 }

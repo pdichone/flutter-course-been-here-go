c2ee97ec8ea3b7902108390d49cf39f93bfe8537
Paulo <pdichone@gmail.com>
2024-02-08 22:36:37 -0800

    Show a google map on screen

--- lib/pages/mapview_places.dart
+++ lib/pages/mapview_places.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class PlacesMapView extends StatefulWidget {
   const PlacesMapView({super.key});
@@ -8,12 +9,23 @@ class PlacesMapView extends StatefulWidget {
 }
 
 class _PlacesMapViewState extends State<PlacesMapView> {
+  late GoogleMapController? mapController;
+
+  Future<void> _onMapCreated(GoogleMapController controller) async {
+    mapController = controller;
+  }
+
   @override
   Widget build(BuildContext context) {
+    const center = LatLng(0.0, 0.0);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Places You\'ve Been'),
       ),
+      body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition:
+              const CameraPosition(target: center, zoom: 10)),
     );
   }
 }

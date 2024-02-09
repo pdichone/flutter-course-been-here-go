d286681e518d5d14cc4dea891946fb9a01f42a94
Paulo <pdichone@gmail.com>
2024-02-08 16:13:35 -0800

    Create a PlacesProvider and a fetchPlaces method

---
+++ lib/providers/places_provider.dart
@@ -0,0 +1,25 @@
+import 'package:been_here_go/models/place.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class PlacesProvider extends ChangeNotifier {
+  FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+  Future<List<Place>> fetchPlaces(String userId) async {
+    try {
+      QuerySnapshot querySnapshot = await _firestore
+          .collection('places')
+          .where('userId', isEqualTo: userId)
+          .get();
+
+      List<Place> places = querySnapshot.docs
+          .map((doc) => Place.fromJson(doc.data() as Map<String, dynamic>))
+          .toList();
+
+      notifyListeners();
+      return places;
+    } catch (error) {
+      throw Exception('Failed to fetch places $error');
+    }
+  }
+}
